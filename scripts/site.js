96d9e3e0e92f83bff747babe59229c1d80d71846
Mathew Sachin <mathew.sachin.git@outlook.com>
2017-08-10 13:11:10 +0530

    dl fix

--- scripts/site.js
+++ scripts/site.js
@@ -2,4 +2,19 @@ $(function()
 {
     $("table").addClass("table table-bordered table-striped table-condensed");
     $("blockquote").addClass("blockquote");
+
+    // Download Link
+    $.getJSON("https://api.github.com/repos/MathewSachin/Captura/releases/latest").done(function(release) {
+        for (var j = 0; j < release.assets.length; ++j)
+        {
+            var asset = release.assets[j];
+
+            if (asset.name == "Setup.exe")
+            {
+                $(".download-link").attr("href", asset.browser_download_url);
+
+                break;
+            }
+        }
+    });
 })
